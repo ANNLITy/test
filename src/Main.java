c82aab035154d58c59fd941307fdd0c9d2990afe
Al040 <klpk44@yandex.ru>
2023-01-10 14:33:42 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        int[] first = new int[3];
+        first[0]=1;
+        first[1]=2;
+        first[2]=3;
+        for (int i = 0; i < first.length; i++) {
+            System.out.println(first[i]);
+            if(i!= first.length-1) {
+                System.out.println(", ");
+            }
+        }
+        System.out.println();
+        for (int i = first.length-1;i>=0; i--) {
+            System.out.println(first[i]);
+            if(i!=0) {
+                System.out.println(", ");
+            }
+        }
     }
 }
